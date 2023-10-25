7592f112073641618895cb2c8b966485741fc04d
Gladibeth <gladibethdelosa@gmail.com>
2023-10-25 19:21:55 -0400

    feat: :sparkles: Add navigation bar and main dashboard structure

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,48 @@
+<template>
+  <Navbar/>
+  <div class="bg-gray-100  h-screen">
+    <div class="max-w-medium w-1920 m-0-auto ">
+      <div class="grid grid-cols-3 gap-4 mb-4 py-10">
+        <div class="bg-white h-48">
+          hola
+        </div>
+        <div class="bg-white h-48">
+          hola
+        </div>
+        <div class="bg-white h-48">
+          hola
+        </div>
+      </div>
+
+      <div class="grid grid-cols-custom gap-4">
+        <div class="bg-white h-48">
+          hola
+        </div>
+        <div class="bg-white h-48">
+          hola
+        </div>
+        <div class="bg-white h-48">
+          hola
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+ 
+ 
+<script>
+import {defineAsyncComponent } from 'vue'
+export default{
+  components: {
+      Navbar: defineAsyncComponent( () => import('../components/Navbar.vue'))
+  },
+  setup(){
+    return{
+
+    }
+  }
+}
+</script>
+<style scoped>
+ 
+</style>

b482e6b37aef1b59d5f3b683a9e877de5de9a00b
Gladibeth <gladibethdelosa@gmail.com>
2023-10-29 21:50:53 -0400

    feat: :zap: Add sidebar in component Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <header class="bg-gradient-to-r from-primary-200 to-primary-900">
     <nav class=" max-w-medium w-1920 m-0-auto py-1 flex justify-between items-center ">
       <div class="flex justify-center">
-        <button class="navbar-burger flex items-center text-blue-600 p-3">
+        <button class="navbar-burger flex items-center text-blue-600 p-3" @click="visibleLeft = true">
           <svg class="block h-5 w-5 " fill="#ffffff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
             <title>Mobile menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
@@ -11,7 +11,7 @@
         <img class="w-50 h-50 " src="../assets/img/logo-av.png">
       </div>
       <div>
-        <h2 class="text-white text-xl font-inter">Dashboard</h2>
+        <h2 class="text-white text-xl font-inter">{{ name }}</h2>
       </div>
       <a class="flex items-center flex-col py-2  text-sm text-white transition duration-200" href="#">
         <div class="flex justify-center px-2 py-2  rounded-full w-35 h-35 bg-white hover:bg-gray-100">
@@ -20,17 +20,8 @@
         <span>Usuario</span>
       </a>
     </nav>
-	<div class="navbar-menu relative z-50 hidden">
-		<div class="navbar-backdrop fixed inset-0 bg-gray-800 opacity-25"></div>
-		<nav class="fixed top-0 left-0 bottom-0 flex flex-col w-5/6 max-w-sm py-6 px-6 bg-white border-r overflow-y-auto">
-			<div class="flex items-center mb-8">
-			
-				<button class="navbar-close">
-					<svg class="h-6 w-6 text-gray-400 cursor-pointer hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-					</svg>
-				</button>
-			</div>
+	
+		<Sidebar v-model:visible="visibleLeft">
 			<div>
 				<ul>
 					<li class="mb-1">
@@ -42,21 +33,30 @@
 					
 				</ul>
 			</div>
-			<div class="mt-auto">
-				<div class="pt-6">
-					<a class="block px-4 py-3 mb-3 leading-loose text-xs text-center font-semibold leading-none bg-gray-50 hover:bg-gray-100 rounded-xl" href="#">Sign in</a>
-					<a class="block px-4 py-3 mb-2 leading-loose text-xs text-center text-white font-semibold bg-blue-600 hover:bg-blue-700  rounded-xl" href="#">Sign Up</a>
-				</div>
-				<p class="my-4 text-xs text-center text-gray-400">
-					<span>Copyright © 2021</span>
-				</p>
-			</div>
-		</nav>
-	</div>
+		</Sidebar>
+		
   </header>
 </template>
+
 <script>
- 
+import { ref } from 'vue'
+import Sidebar from 'primevue/sidebar'
+ export default{
+	components:{
+		Sidebar
+	},
+	props: {
+		name: {type: String, required: true}
+	},
+	setup(){
+		const visibleLeft = ref(false);
+
+		return{
+			visibleLeft
+		}
+	}
+ }
 </script>
+
 <style>
 </style>
